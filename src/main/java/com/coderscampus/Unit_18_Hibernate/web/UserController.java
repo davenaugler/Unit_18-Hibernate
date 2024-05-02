1f4bddc266b1f4341774753f4eb079ac29f6f428
Dave Naugler <davenaugler@gmail.com>
2024-05-02 13:32:13 -0600

    Unit 18 - Updates
    Updated UI to a dark UI. Name of user is now clickable using Thymeleaf and will navigate to their content. Added All Users buttons to navigate back to view all the Users.

--- src/main/java/com/coderscampus/Unit_18_Hibernate/web/UserController.java
+++ src/main/java/com/coderscampus/Unit_18_Hibernate/web/UserController.java
@@ -33,7 +33,15 @@ public class UserController {
     public String getOneUser(ModelMap model, @PathVariable Long userId) {
         User user = userService.findById(userId);
         model.put("users", Arrays.asList(user));
+        // Code smell on line below
+        model.put("user", user);
         return "users";
+    }
+
+    @PostMapping("/users/{userId}")
+    public String postOneUser(@PathVariable Long userId, User user) {
+        userService.saveUser(user);
+        return "redirect:/users/" + user.getUserId();
     }
 
     @GetMapping("/register")
@@ -45,7 +53,7 @@ public class UserController {
     @PostMapping("/register")
     public String postCreateUser(User user) {
         System.out.println("user: " + user);
-        userService.createUser(user);
+        userService.saveUser(user);
         return "redirect:/register";
     }
 
